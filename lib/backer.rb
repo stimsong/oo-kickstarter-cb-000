837416845e896bf0c1b960a04fe2e6f74b4a10ee
Greg Stimson <stimsong@yahoo.com>
2018-04-06 20:38:22 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,14 @@
 class Backer
   attr_accessor :name, :backed_projects
 
-  @backed_projects = []
 
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
 
   def back_project(project)
-    self.backed_projects << project
+    backed_projects << project
   end
 
 end
